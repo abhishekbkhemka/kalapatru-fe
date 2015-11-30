d9a57675f19ea88b99d07e27d7578c91415a38de
Abhishek Khemka <abhishekbkhemka@gmail.com>
2015-11-30 19:10:29 +0530

    wip

--- com/js/config.js
+++ com/js/config.js
@@ -1,11 +1,12 @@
 'use strict'
 
-var PROD = 'www.kalapatru.com:8080/'
+var PROD = 'http://ec2-54-169-108-233.ap-southeast-1.compute.amazonaws.com:8000/'
 var DEV = 'http://127.0.0.1:8080/'
-var GET_TRASPORTERS = DEV+'api/transporters/'
-var GET_CUSTOMERS = DEV+'api/customers/'
-var FORWARDING_NOTE = DEV+'api/forwardingNote/'
-var FORWARDING_NOTES = DEV+'api/forwardingNotes/'
-var DISPATCH = DEV+'api/dispatch/'
+var CURRENT = DEV;
+var GET_TRASPORTERS = CURRENT+'api/transporters/'
+var GET_CUSTOMERS = CURRENT+'api/customers/'
+var FORWARDING_NOTE = CURRENT+'api/forwardingNote/'
+var FORWARDING_NOTES = CURRENT+'api/forwardingNotes/'
+var DISPATCH = CURRENT+'api/dispatch/'
 var HTTP_401 = 401
 var HTTP_403 = 403
