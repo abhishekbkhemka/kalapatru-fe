123ae680aac36a6a8c7bcc6c6a53bc00c576cb4c
Abhishek Khemka <abhishekbkhemka@gmail.com>
2015-12-04 19:34:46 +0530

    fix

--- app/dispatches/dispaches.js
+++ app/dispatches/dispaches.js
@@ -11,6 +11,7 @@ angular.module('myApp.dispatches', ['ngRoute'])
 
     .controller('DispatchesCtrl', ['$scope','kalapatruService',function($scope,kalapatruService) {
         $scope.dispacthes = []
+        $scope.visibleForDisplay = true
 
         $scope.getDispatches = function(fromDate,toDate){
             showLoadingBar()
@@ -45,23 +46,52 @@ angular.module('myApp.dispatches', ['ngRoute'])
         }
 
         $scope.print = function(){
+            $scope.visibleForDisplay = false
+
             $('#print_btn_id').hide()
             $('#close_btn_id').hide()
-            html2canvas([ document.getElementById('vanDetails_id') ], {
-                onrendered: function (canvas) {
-                    var myImage = canvas.toDataURL("image/png");
-                    var printWin = window.open();
-                    //printWin.moveTo(200, 100);
-                    printWin.document.write('<img src="' + myImage + '"/>');
-                    printWin.focus();
-                    printWin.print();
-                    printWin.close();
-                    $('#print_btn_id').show()
-                    $('#close_btn_id').show()
-                    return
-                }
+            setTimeout(function(){
+                //var scaledElement = $("#vanDetails_id").clone().css({
+                //    'transform': 'scale(3,3)',
+                //    '-ms-transform': 'scale(3,3)',
+                //    '-webkit-transform': 'scale(3,3)'
+                //});
+                //var oldWidth = scaledElement.width();
+                //var oldHeight = scaledElement.height();
+                //
+                //var newWidth = 1000// oldWidth * (1/3);
+                //var newHeight = 1000//oldHeight * (1/3);
+                //html2canvas(document.getElementById('vanDetails_id'), {
+                //    onrendered: function(canvasq) {
+                //        var w=window.open();
+                //        //w.document.write("<h3 style='text-align:center;'>"+ReportTitle+"</h3>");
+                //        w.document.write("<img width='"+newWidth+"' height='"+newHeight+"' src='"+canvasq.toDataURL()+"' />");
+                //        w.print();
+                //    }
+                //});
 
-            })
+                //console.log(oldHeight)
+                //console.log(newWidth)
+
+                html2canvas([ document.getElementById('vanDetails_id') ], {
+                    onrendered: function (canvas) {
+                        var myImage = canvas.toDataURL("image/png");
+                        var printWin = window.open();
+                        //printWin.moveTo(200, 100);
+                        printWin.document.write("<img  src='" + myImage + "'/>");
+                        printWin.focus();
+                        printWin.print();
+                        printWin.close();
+                        $('#print_btn_id').show()
+                        $('#close_btn_id').show()
+                        $scope.visibleForDisplay = true
+                        $scope.$apply()
+                        return
+                    }
+
+                })
+            },500)
+
         }
 
 
