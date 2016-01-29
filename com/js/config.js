46152ebc3210349059d00bb677e2c2f7f931346a
Abhishek Khemka <abhishekbkhemka@gmail.com>
2016-01-29 19:49:16 +0530

    fixes

--- com/js/config.js
+++ com/js/config.js
@@ -1,14 +1,15 @@
 'use strict'
 
 var PROD = 'http://ec2-54-169-108-233.ap-southeast-1.compute.amazonaws.com:8000/'
-var DEV = 'http://127.0.0.1:8080/'
-var CURRENT = PROD;
+var DEV = 'http://127.0.0.1:8000/'
+var CURRENT = DEV;
 var GET_TRASPORTERS = CURRENT+'api/transporters/'
 var GET_CUSTOMERS = CURRENT+'api/customers/'
 var FORWARDING_NOTE = CURRENT+'api/forwardingNote/'
 var SETTINGS = CURRENT+'api/settings/'
 var FORWARDING_NOTES = CURRENT+'api/forwardingNotes/'
 var DISPATCH = CURRENT+'api/dispatch/'
+var VANS = CURRENT+'api/vans/'
 var DISPATCHES = CURRENT+'api/dispatches/'
 var HTTP_401 = 401
 var HTTP_403 = 403
